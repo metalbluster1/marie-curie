752ef468c31c559a719ecd12dc68794b247d74f7
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-11-11 13:38:34 +0530

    1. Implemented Javascript to scroll across sections instead of a elements. 2. added favicon.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,5 @@
+/* Hamburger icon for mobile screen */
+
 const hamburger = document.getElementById("hamburger");
 const topBar = document.querySelectorAll(".top-bar")[0];
 const middleBar = document.querySelectorAll(".middle-bar")[0];
@@ -11,3 +13,32 @@ hamburger.addEventListener('click', (e) => {
     bottomBar.classList.toggle("bottom-bar--active");
     navBarLinks.classList.toggle("nav-bar-links--is-visible");
 })
+
+
+/* Scroll to section using navigatin bar */
+
+let mainTopPaddingString = window.getComputedStyle(document.getElementById("main__section")).paddingTop;
+let mainTopPadding = removePX(mainTopPaddingString);
+
+let headerHeightString = window.getComputedStyle(document.getElementById("header")).height;
+let headerHeight = removePX(headerHeightString);
+
+let imageBanner = document.getElementById("img-banner");
+let moveToSection = document.querySelectorAll(".move-to-section");
+
+moveToSection.forEach((el) => {
+    el.addEventListener('click', () => {
+        let imagePositionY = imageBanner.getBoundingClientRect().y + headerHeight - mainTopPadding;
+        let id = el.id;
+        let idPositionY = document.getElementById(`${id}__section`).getBoundingClientRect().y;
+        
+        window.scrollTo(0, idPositionY - imagePositionY);
+    })
+})
+
+/* Removes pixels from getComputedStyleValues */
+
+function removePX(value) {
+    let loc = value.indexOf('p');
+    return parseInt(value.slice(0, loc));
+}
